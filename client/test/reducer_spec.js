9a216fa00141445b78403f0cc34b033af09c3e4b
David Smith <davidsmith2@gmail.com>
2016-10-30 17:11:54 -0400

    Fix reducer spec

--- client/test/reducer_spec.js
+++ client/test/reducer_spec.js
@@ -4,21 +4,72 @@ import {reducer} from '../src/reducer';
 
 describe('reducer', () => {
 
-  it('handles GUESS', () => {
-  	expect(reducer({
-      currentGuess: '-',
-      guessAccuracy: '-',
-      guessesMade: 0,
-      secretNumber: 1
-    }, {
-      type: 'GUESS',
-      number: 2
-    })).to.equal({
-      currentGuess: 2,
-      guessAccuracy: 'High',
-      guessesMade: 1,
-      secretNumber: 1
-  	});
-  });
+    describe('GUESS', () => {
+
+        it('sets current guess', () => {
+            const state = {
+                currentGuess: '-'
+            };
+            const action = {
+                type: 'GUESS',
+                number: 2
+            };
+            const nextState = reducer(state, action);
+            expect(nextState.currentGuess).to.equal(2);
+        });
+
+        it('increments guesses made', () => {
+            const state = {
+                guessesMade: 0
+            };
+            const action = {
+                type: 'GUESS',
+                number: 2
+            };
+            const nextState = reducer(state, action);
+            expect(nextState.guessesMade).to.equal(1);
+        });
+
+        describe('guess accuracy', () => {
+
+            it('handles high guesses', () => {
+                const state = {
+                    secretNumber: 1
+                };
+                const action = {
+                    type: 'GUESS',
+                    number: 2
+                };
+                const nextState = reducer(state, action);
+                expect(nextState.guessAccuracy).to.equal('High');
+            });
+
+            it('handles low guesses', () => {
+                const state = {
+                    secretNumber: 2
+                };
+                const action = {
+                    type: 'GUESS',
+                    number: 1
+                };
+                const nextState = reducer(state, action);
+                expect(nextState.guessAccuracy).to.equal('Low');
+            });
+
+            it('handles correct guesses', () => {
+                const state = {
+                    secretNumber: 2
+                };
+                const action = {
+                    type: 'GUESS',
+                    number: 2
+                };
+                const nextState = reducer(state, action);
+                expect(nextState.guessAccuracy).to.equal('Match');
+            });
+
+        });
+
+    });
 
 });
